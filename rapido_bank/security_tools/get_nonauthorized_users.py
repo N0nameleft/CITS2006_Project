256f2cdd9ac3ca4f68f592475ee38bf4e109788b
Chowewow <chowewoow@gmail.com>
2024-05-16 09:52:57 +0800

    added get_nonauthorized_users.py

---
+++ rapido_bank/security_tools/get_nonauthorized_users.py
@@ -0,0 +1,28 @@
+import pwd
+
+def get_system_users():
+    """Retrieve a list of all users on the system."""
+    users = [user.pw_name for user in pwd.getpwall()]
+    return users
+
+def check_authorized_users(authorized_users):
+    """Check for non-authorized users on the system."""
+    system_users = get_system_users()
+    non_authorized_users = [user for user in system_users if user not in authorized_users]
+    
+    if non_authorized_users:
+        print("Non-authorized users found:")
+        for user in non_authorized_users:
+            print(f"- {user}")
+    else:
+        print("No non-authorized users found. All users are authorized.")
+
+if __name__ == "__main__":
+    authorized_users = [
+        'admin', 'charles', 'mathilde', 'diego', 'santiago', 'maria', 'maxwell',
+        'root', 'daemon', 'bin', 'sys', 'sync', 'games', 'man', 'lp', 'mail',
+        'news', 'uucp', 'proxy', 'www-data', 'backup', 'list', 'irc', '_apt',
+        'nobody', 'ubuntu'
+    ]
+    check_authorized_users(authorized_users)
+
